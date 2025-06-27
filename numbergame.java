cabb900476805fba29e90d9e36faf12918119693
Manisha <manisha22704@gmail.com>
2025-06-27 23:45:11 +0530

    numbergame.java

--- numbergame.java
+++ numbergame.java
@@ -1,12 +1,9 @@
-package number_game;
-//import java.util.Random;
 import java.util.*;
 public class numbergame {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         Random random=new Random();
         int randomNumber = random.nextInt(100) + 1;
-//        System.out.println("Random number between 1 and 100: " + randomNumber);
         System.out.println("***--------WELCOME TO NUMBER GAME --------***");
         int count=0;
         for(int i=1;i<=3;i++){
@@ -22,9 +19,6 @@ public class numbergame {
 
             }
         }
-//        System.out.println();
-//        System.out.println("Congratulation you win \n"+"Your score is : "+count);
-
         for (int i=1;i<5;i++) {
             System.out.print("IF YOU WANT TO EXIT THE GAME ENTER  1   AND IF YOU WANT TO CONTINUE THE GAME PLEASE PREE 2 : ");
             int p = sc.nextInt();
